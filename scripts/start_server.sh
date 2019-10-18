c180c8546854aff80dff3978d699b3c936843296
Max <maksymkhrystenko@gmail.com>
2019-10-18 16:37:03 +0300

    codedelpoy

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@ _#!/bin/bash_
 export PM2_HOME=/home/ubuntu/.pm2
 pm2 delete prana-club
 cd /home/ubuntu/prana-club/
-pm2 start --name prana-club npm -- start
+pm2 start --name prana-club npm -- start --watch
